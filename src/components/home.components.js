1394d02618b884bd0c988063a1eee75d7c0ca3d3
Zhao Chen <Jchen1117@gmail.com>
2020-07-30 14:33:31 -0400

    working navbar

---
+++ src/components/home.components.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import NavigationBar from "./navbar.components";
+
+export default class Home extends Component{
+    render(){
+        return (
+            <div>
+            <NavigationBar/>
+            </div>
+        )}
+}
---
+++ src/components/navbar.components.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {Navbar, Nav, Form, FormControl, Button} from "react-bootstrap";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css'
+
+export default class NavigationBar extends Component{
+    render(){
+        return (
+            <Navbar sticky="top" expand="lg" style={{backgroundColor:"#f1faee"}}>
+
+                <Navbar.Brand href="/"><img src={require("../image/One_Drop_Shop.png")}  className="d-inline-block align-top" alt="Website logo" style={{padding: "5px",width:"300px", maxWidth:"100%"}}/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto" style={{fontSize:"20px"}}>
+                        <a href="#New"className="Nav-Links">New Releases</a>
+                        <a href="#Sale"className="Nav-Links">On Sale</a>
+                        <a href="#Shop"className="Nav-Links">Shop</a>
+                    </Nav>
+                    <div className="cart">
+                        <div className="cart-red">
+                            <div style={{textAlign:"center", color:"white", lineHeight: "20px"}} >1</div>
+                        </div>
+                    </div>
+                </Navbar.Collapse>
+            </Navbar>
+        )}
+}
